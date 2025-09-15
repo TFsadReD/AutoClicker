b29f3380eb9a6976169a374e9da1d44249e09708
TFsadReD <MaiK.HeliO@yandex.ru>
2025-09-15 13:43:39 +0300

    1.0 - it's work

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 from pyautogui import click
 from time import sleep
-from keyboard import add_hotkey, wait
+from keyboard import is_pressed
 
 positions = [(1349, 995), (1812, 995)]
 delay = 0.1
 running = False
+i = 0
 
-def clicker():
-    global running
-    i = 0
-    while True:
-        if running:
-            x, y = positions[i % len(positions)]
-            click(x, y)
-            print(f"Клик {i+1} по координатам: {x}, {y}")
-            i += 1
-            sleep(delay)
-        else:
+ver = "Release 1.0"
+print(f"AutoClicker {ver}\nНажми F8 чтобы ЗАПУСТИТЬ/ОСТАНОВИТЬ кликер, ESC чтобы выйти")
+
+while True:
+    if is_pressed("esc"):
+        print(">>> Выход из программы...")
+        break
+
+    if is_pressed("F8"):
+        running = not running
+        print(">>> Кликер ВКЛЮЧЕН" if running else ">>> Кликер ВЫКЛЮЧЕН")
+        while is_pressed("F8"):
             sleep(0.1)
 
-print("Нажми F8 чтобы ЗАПУСТИТЬ/ОСТАНОВИТЬ кликер, ESC чтобы выйти.")
-
-add_hotkey("F8", lambda: globals().__setitem__("running", not running))
-
-wait("esc")
+    if running:
+        x, y = positions[i % len(positions)]
+        click(x, y)
+        print(f">>> Клик {i+1} по координатам: {x}, {y}")
+        i += 1
+        sleep(delay)
+    else:
+        sleep(0.1)
